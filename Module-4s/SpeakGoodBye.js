3323a1d4bcae4e28d9c7f68264f2a46a3ca309ac
Yeoj216 <joeyghmdp@gmail.com>
2020-10-25 23:40:19 +0200

    ghabe

--- Module-4s/SpeakGoodBye.js
+++ Module-4s/SpeakGoodBye.js
@@ -17,10 +17,10 @@ var byeSpeaker = {
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-speak:function (name){
+speak: function (name){
   console.log(speakWord + " " + name);
 }};
-
+window.byeSpeaker = byeSpeaker;
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
